72f2ecd9d25818f72a868da1ec1bfe08a8f0c77b
omprkash kumar <112754995+omprakash8788@users.noreply.github.com>
2025-02-02 08:54:07 +0530

    Update f.js

--- problem21/f.js
+++ problem21/f.js
@@ -1,4 +1,5 @@
 // A subprocedure or void function in JavaScript.
+// In JavaScript, a subprocedure (also known as a void function) performs an operation without returning a value.
 function curve(arr, amount) {
   for (var i = 0; i < arr.length; ++i) {
     arr[i] += amount;
@@ -7,3 +8,4 @@ function curve(arr, amount) {
 var grades = [77, 73, 74, 81, 90];
 curve(grades, 5);
 print(grades);
+// Expected output- [ 82, 78, 79, 86, 95 ]
