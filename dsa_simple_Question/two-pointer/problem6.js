68de2888f94faf440fd00a54d254f5a9444aaab9
omprakash8788 <omprakashkumar552@gmail.com>
2025-04-18 08:51:04 +0530

    ps

---
+++ dsa_simple_Question/two-pointer/problem6.js
@@ -0,0 +1,13 @@
+/*
+  Three Sum
+
+  Given an array A, find all unique triplets in the array whose sum is equal to zero.
+
+  Example:
+A: [1, 1, 0, -1, -2]
+Triplets: [
+  [-2, 1, 1],
+  [-1, 0, 1]
+]
+Note: Each triplet should be sorted. The resultant array should be sorted as well.
+*/
