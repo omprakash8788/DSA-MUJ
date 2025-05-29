54b4c5685bcf575c1352e7ed4723234f4e882fce
omprakash8788 <omprakashkumar552@gmail.com>
2025-05-29 08:31:29 +0530

    dp p1

---
+++ dp/problem1.js
@@ -0,0 +1,35 @@
+/*
+ Nth Fibonacci Number
+
+ Given a positive integer n, the task is to find the nth Fibonacci number.
+
+The Fibonacci sequence is a sequence where the next term is the sum of the previous two terms. The first two terms of the Fibonacci sequence are 0 followed by 1. The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
+
+Example:
+
+Input: n = 2
+Output: 1 
+Explanation: 1 is the 2nd number of Fibonacci series.
+
+Input: n = 5
+Output: 5
+Explanation: 5 is the 5th number of Fibonacci series.
+
+
+*/
+
+function nthFibonacci(n){
+
+    // Base case: if n is 0 or 1, return n
+    if (n <= 1) {
+        return n;
+    }
+    
+    // Recursive case: sum of the two preceding Fibonacci
+    // numbers
+    return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+}
+
+let n = 5;
+let result = nthFibonacci(n);
+console.log(result);
