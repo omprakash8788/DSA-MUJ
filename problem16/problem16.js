01be3f951978e4c6ecfc8907f153b0ea0608a708
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2025-06-10 22:56:26 +0530

    Update problem16.js

--- problem16/problem16.js
+++ problem16/problem16.js
@@ -11,10 +11,22 @@ Explanation:
 n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
  */
 
-let nums=[3,0,1]
-let total=nums.length
-for(let i=0; i<nums.length; i++){
-    total=total+i-nums[i]
+// let nums=[3,0,1]
+// let total=nums.length
+// for(let i=0; i<nums.length; i++){
+//     total=total+i-nums[i]
 
+// }
+// console.log("Final output",total);
+
+let n = nums.length + 1;
+// console.log(n);
+let sumofN = (n * (n + 1)) / 2;
+// console.log(sumofN);
+let sumofArray = 0;
+for (let i = 0; i < nums.length; i++) {
+  sumofArray = sumofArray + nums[i];
 }
-console.log("Final output",total);
+// console.log(sumofArray);
+let missing_number = sumofN - sumofArray;
+console.log(missing_number);
