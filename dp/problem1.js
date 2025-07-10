418ffd7fade5a2110a380fb7fa222d93df8d01aa
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2025-07-11 00:08:23 +0530

    Update problem1.js

--- dp/problem1.js
+++ dp/problem1.js
@@ -1,6 +1,5 @@
 /*
  Nth Fibonacci Number
- 
  Given a positive integer n, the task is to find the nth Fibonacci number.
 The Fibonacci sequence is a sequence where the next term is the sum of the previous two terms. The first two terms of the Fibonacci sequence are 0 followed by 1. The Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21
 Example:
