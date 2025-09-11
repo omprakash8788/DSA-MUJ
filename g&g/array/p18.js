f3e2cfeaa33a017b7de6a82b0f004c47302ac11f
omprakash kumar <112754995+omprakash8788@users.noreply.github.com>
2025-09-11 22:03:12 +0530

    Update p18.js

--- g&g/array/p18.js
+++ g&g/array/p18.js
@@ -1,3 +1,4 @@
+
 // Common in 3 Sorted Arrays
 
 // Given three sorted arrays in non-decreasing order, print all common elements in non-decreasing order across these arrays. If there are no such elements return an empty array. In this case, the output will be -1.
