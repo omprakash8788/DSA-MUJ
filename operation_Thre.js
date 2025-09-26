59e55bd805310b43b7489192b8b569e2691109cf
omprakash8788 <omprakashkumar552@gmail.com>
2025-09-26 10:14:15 +0530

    np46

--- operation_Thre.js
+++ operation_Thre.js
@@ -1,13 +1,11 @@
-let nums = [1,3,4,8,7,9,3,5,1], k = 2
-
-// Output: [[1,1,3],[3,4,5],[7,8,9]]
-nums.sort((a,b)=>(a-b));
-// let n=nums.length/3
-let three=[]
+let nums = [2,1,3,3], k = 2
+ let arr=[]
 for(let i=0; i<nums.length; i++){
-    if(nums[i] <= k){
-        three.push(nums[i])
+    let temp=[];
+    for(let j=i+1; j<nums.length; j++){
+        temp.push(nums[i], nums[j])
+
     }
-  
+    
+   
 }
-console.log(three)
