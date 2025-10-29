fb31b8defd6afa0cc321bb434e979d5e3ac5edd4
omprakash8788 <omprakashkumar552@gmail.com>
2025-10-29 08:31:04 +0530

    p68

---
+++ g&g/array/p69.js
@@ -0,0 +1,26 @@
+// 1287. Element Appearing More Than 25% In Sorted Array
+
+// Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+
+// Example 1:
+
+// Input: arr = [1,2,2,6,6,6,6,7,10]
+// Output: 6
+// Example 2:
+
+// Input: arr = [1,1]
+// Output: 1
+
+let arr = [1, 2, 2, 6, 6, 6, 6, 7, 10];
+var findSpecialInteger = function (arr) {
+  const n = arr.length;
+  const threshold = Math.floor(n / 4);
+//   console.log("op",threshold); // 2
+
+  for (let i = 0; i < n - threshold; i++) {
+    if (arr[i] === arr[i + threshold]) {
+      return arr[i];
+    }
+  }
+};
+console.log(findSpecialInteger(arr));
