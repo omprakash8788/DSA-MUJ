06f44d925f89c7d3079ead3ca1818d17f86bab46
omprakash8788 <omprakashkumar552@gmail.com>
2025-11-19 20:46:23 +0530

    hr6

---
+++ interview-Question/hof/reduce/p1.js
@@ -0,0 +1,10 @@
+// 🚀 MASTERCLASS: JavaScript .reduce() — ALL SCENARIOS
+// ✅ 1. Basic Syntax
+// arr.reduce((accumulator, currentValue, index, array) => { ... }, initialValue)
+
+// ✅ 2. Scenario: Sum of numbers
+
+// → 10
+let arr =[1,2,3,4]
+let ans = arr.reduce((acc, cur) => acc + cur, 0)
+console.log(ans)
---
+++ interview-Question/hof/reduce/p4.js
@@ -0,0 +1,4 @@
+// 4. Scenario: Convert array of strings to one string
+let arr = ["a", "b", "c"];
+let ans = arr.reduce((acc, val)=>(acc + val))
+console.log(ans)
